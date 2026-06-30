5fbc732147329550529e7f6642f83f7a4867fb85
mzaidiii <mohdmurtaza153@gmail.com>
2026-07-01 00:07:36 +0530

    Service layer for auth

---
+++ lib/features/auth/data/services/auth_service.dart
@@ -0,0 +1,83 @@
+import 'package:dio/dio.dart';
+
+import 'package:pawffy/core/networks/dio_client.dart';
+import 'package:pawffy/core/networks/api_constants.dart';
+import '../models/auth_response_model.dart';
+import 'package:pawffy/features/auth/data/models/user_model.dart';
+
+class AuthService {
+  final Dio _dio = DioClient.dio;
+
+  Future<AuthResponseModel> login({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final response = await _dio.post(
+        ApiConstants.login,
+        data: {'email': email, 'password': password},
+      );
+
+      return AuthResponseModel.fromJson(response.data);
+    } on DioException catch (e) {
+      throw Exception(e.response?.data['message'] ?? 'Login failed');
+    }
+  }
+
+  Future<UserModel> getMe(String token) async {
+    try {
+      final response = await _dio.get(
+        ApiConstants.me,
+        options: Options(headers: {'Authorization': 'Bearer $token'}),
+      );
+
+      return UserModel.fromJson(response.data['data']);
+    } on DioException catch (e) {
+      throw Exception(e.response?.data['message'] ?? 'Failed to fetch profile');
+    }
+  }
+
+  Future<String> changePassword({
+    required String currentPassword,
+    required String newPassword,
+    required String token,
+  }) async {
+    try {
+      final response = await _dio.post(
+        ApiConstants.changePassword,
+        data: {'currentPassword': currentPassword, 'newPassword': newPassword},
+        options: Options(headers: {'Authorization': 'Bearer $token'}),
+      );
+
+      return response.data['data']['token'] as String;
+    } on DioException catch (e) {
+      throw Exception(
+        e.response?.data['message'] ?? 'Failed to change password',
+      );
+    }
+  }
+
+  Future<String> forgotPassword({required String email}) async {
+    try {
+      final response = await _dio.post(
+        ApiConstants.forgotPassword,
+        data: {'email': email},
+      );
+      return response.data['message'] as String? ??
+          'Password reset link sent to your email';
+    } on DioException catch (e) {
+      throw Exception(e.response?.data['message'] ?? 'Forgot password failed');
+    }
+  }
+
+  Future<void> logout(String token) async {
+    try {
+      await _dio.post(
+        ApiConstants.logout,
+        options: Options(headers: {'Authorization': 'Bearer $token'}),
+      );
+    } on DioException catch (e) {
+      throw Exception(e.response?.data['message'] ?? 'Logout failed');
+    }
+  }
+}
