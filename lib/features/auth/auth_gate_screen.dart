075d2d43831b29279da2813f874fe23f5fc808a2
mzaidiii <mohdmurtaza153@gmail.com>
2026-07-01 00:08:45 +0530

    Auth Gate to check token

---
+++ lib/features/auth/auth_gate_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pawffy/features/auth/Onboarding_Screen.dart';
+
+import '../home/home_screen.dart';
+import 'providers/auth_controller.dart';
+
+class AuthGateScreen extends ConsumerStatefulWidget {
+  const AuthGateScreen({super.key});
+
+  @override
+  ConsumerState<AuthGateScreen> createState() => _AuthGateScreenState();
+}
+
+class _AuthGateScreenState extends ConsumerState<AuthGateScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _checkAuth();
+  }
+
+  Future<void> _checkAuth() async {
+    try {
+      final user = await ref.read(authControllerProvider.notifier).getMe();
+
+      if (!mounted) return;
+
+      if (user != null) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (_) => const HomeScreen()),
+        );
+      } else {
+        _goToOnboarding();
+      }
+    } catch (_) {
+      if (!mounted) return;
+      _goToOnboarding();
+    }
+  }
+
+  void _goToOnboarding() {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (_) => const OnboardingScreen()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(child: CircularProgressIndicator(color: Color(0xFFE85D04))),
+    );
+  }
+}
