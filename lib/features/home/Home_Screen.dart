276009cd9dd144320a96250485556ea87d3d75f4
mzaidiii <mohdmurtaza153@gmail.com>
2026-07-01 00:10:38 +0530

    Project Setup

---
+++ lib/features/home/Home_Screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+  @override
+  State<HomeScreen> createState() {
+    return _HomeScreen();
+  }
+}
+
+class _HomeScreen extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('Hello ')));
+  }
+}
