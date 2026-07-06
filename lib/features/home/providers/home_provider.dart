82a9d1b413f98e6081f86a203f3bb1adab37f074
mzaidiii <mohdmurtaza153@gmail.com>
2026-07-06 20:07:51 +0530

    Updated Provider ( More Structure )

--- lib/features/home/providers/home_provider.dart
+++ lib/features/home/providers/home_provider.dart
@@ -4,3 +4,16 @@ import '../data/services/home_service.dart';
 final homeServiceProvider = Provider<HomeService>((ref) {
   return HomeService();
 });
+
+class NavigationIndexNotifier extends Notifier<int> {
+  @override
+  int build() => 0;
+
+  void setIndex(int index) {
+    state = index;
+  }
+}
+
+final navigationIndexProvider = NotifierProvider<NavigationIndexNotifier, int>(
+  NavigationIndexNotifier.new,
+);
