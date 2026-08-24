5b27a99ba6806355a36e09b3c920ea6934285e10
mzaidiii <mohdmurtaza153@gmail.com>
2026-08-24 16:49:12 +0530

    new Supabase Config

--- lib/core/config/supabase_config.dart
+++ lib/core/config/supabase_config.dart
@@ -1,6 +1,7 @@
 class SupabaseConfig {
-  static const String url = 'https://hnwslusckrzbnulxwgwp.supabase.co';
-  static const String anonKey = 'sb_publishable_nuKdWOHmpNp87YsximpVVw_1Huju3QL'; 
+  static const String url = 'https://srnqlmsmuuurlcrrgywm.supabase.co';
+  static const String anonKey =
+      ' sb_publishable_hkgdmP70S50oSrWo7P47fw_D4eAZjJ2';
 
   static const bool useMockAuth = false;
 }
